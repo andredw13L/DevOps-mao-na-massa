84e682744d39cf0fdb4d19a625687d8f5efc531b
andredw13L <andreribeiro13l@gmail.com>
2023-09-03 20:17:51 -0300

    update lab-swarm

---
+++ lab-swarm/provision.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo yum install -y nano
+
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
