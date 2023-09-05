18fd1aced96726bdafac27a04cab8299e806cb6a
andredw13L <andreribeiro13l@gmail.com>
2023-09-05 00:42:49 -0300

    Adicionado o lab-jenkins

---
+++ lab-jenkins/provision.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+
+yum install -y epel-release wget java-11-openjdk-devel git
+
+#sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
+#sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+#yum install -y jenkins
+
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install jenkins -y
+
+systemctl daemon-reload
+service jenkins start
+
+# Instalação do docker e do docker compose
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo yum install -y nano
+
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+systemctl daemon-reload
+systemctl restart docker
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+usermod -aG docker jenkins
