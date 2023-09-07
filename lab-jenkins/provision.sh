096ca5906a928dc2ede778f8156b9bdf1dc590db
andredw13L <andreribeiro13l@gmail.com>
2023-09-07 01:39:36 -0300

    Adicionado sonar-scanner e node.js

--- lab-jenkins/provision.sh
+++ lab-jenkins/provision.sh
@@ -2,6 +2,8 @@
 
 yum install -y epel-release wget java-11-openjdk-devel git telnet
 
+
+
 #sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 #sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 #yum install -y jenkins
@@ -31,6 +33,27 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 systemctl daemon-reload
 systemctl restart docker
+
+
+# Instalar sonar scanner
+
+yum install -y  unzip
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
+
+unzip sonar-scanner-cli-4.6.2.2472-linux.zip -d /opt/
+
+mv /opt/sonar-scanner-4.6.2.2472-linux /opt/sonar-scanner
+
+chown -R jenkins:jenkins /opt/sonar-scanner
+
+echo "export PATH=$PATH:/opt/sonar-scanner/bin" | sudo tee -a /etc/profile
+
+# Instalar Node.js
+
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+
+sudo yum install nodejs -y
 
 
 
