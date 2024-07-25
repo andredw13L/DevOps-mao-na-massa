7ce5b83d08a70917c10231d8f299ab1a7a81c064
andredw13L <andreribeiro13l@gmail.com>
2024-07-25 05:11:21 -0300

    Configuração do sonar-scanner e plugins no Jenkins

--- lab-jenkins/provision.sh
+++ lab-jenkins/provision.sh
@@ -42,3 +42,30 @@ sudo systemctl daemon-reload
 sudo usermod -aG docker jenkins
 
 sudo systemctl restart jenknis
+
+# Instalação do sonar scanner
+
+sudo apt-get install wget unzip -y
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-6.1.0.4477-linux-x64.zip
+
+unzip sonar-scanner-cli-6.1.0.4477-linux-x64.zip -d /opt/
+
+mv /opt/sonar-scanner-6.1.0.4477-linux-x64 /opt/sonar-scanner
+
+chown -R jenkins:jenkins /opt/sonar-scanner
+
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+
+
+# Instalação do Node.js
+
+sudo apt-get install -y curl
+
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+
+sudo -E bash nodesource_setup.sh
+
+sudo apt-get install -y nodejs
+
+node -v
